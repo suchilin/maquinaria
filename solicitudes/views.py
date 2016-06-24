ca35802c5ecf0f4f1d27a5f3f7d81118498fdc42
unknown <jose.gzapata@130-60VQR.SAGARPA.NET>
2016-06-24 09:09:46 -0500

    tird change

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -92,7 +92,11 @@ def updateperfil(request):
 @login_required
 def ccdd(request, solicitud_id=None):
     solicitud = Solicitudes.objects.get(pk=solicitud_id)
-    return render(request, 'ccdd.html', {'solicitud':solicitud})
+    mes_ = {1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"}
+    fecha = date.today()
+    dia = fecha.day
+    mes = mes_[fecha.month]
+    return render(request, 'ccdd.html', {'solicitud':solicitud, 'dia':dia, 'mes':mes})
 
 
 @login_required
